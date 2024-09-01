bbdf68fa2a24c6b4aa5ab294cf313854760c875e
liorhassid <lior.hassid@gmail.com>
2024-09-01 22:37:39 +0300

    Add project files.

---
+++ SocketsPingPong2/Program.cs
@@ -0,0 +1,11 @@
+﻿using SocketsPingPong2;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ServerSide serverSide = new ServerSide();
+        serverSide.StartServer();
+    }
+}
